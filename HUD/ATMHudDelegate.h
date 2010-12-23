126c67c501f602bdf5ea16e1e7cc786e82d48c67
Matthias Tretter <matthias.tretter@gmail.com>
2010-12-23 16:59:03 +0100

    added AtomHUD (http://www.atomcraft.de/blog/2010/12/23/atomhud-release/)

---
+++ HUD/ATMHudDelegate.h
@@ -0,0 +1,65 @@
+/*
+ *  ATMHudDelegate.h
+ *  atomHUD
+ *
+ *  Created by Marcel Müller on 25.11.10.
+ *  Copyright (c) 2010, Marcel Müller (atomcraft)
+ *  All rights reserved.
+ *
+ */
+
+#import <UIKit/UIKit.h>
+@class ATMHud;
+
+/**
+	The delegate of a ATMHud object can adopt the ATMHudDelegate protocol. Optional methods of the protocol allow the delegate to manage touches and get notified about the view state.
+ */
+@protocol ATMHudDelegate
+
+@optional
+/**
+	Called when the user taps the HUD.
+	Can be blocked with the blockTouches property.
+	@param _hud The hud which has been tapped.
+ */
+- (void)userDidTapHud:(ATMHud *)_hud;
+
+/**
+	Called before the HUD appears.
+	This is the place where you can call setCenter: to prevent the HUD from being centered within its superview.
+	@param _hud The hud which will appear.
+ */
+- (void)hudWillAppear:(ATMHud *)_hud;
+
+/**
+	Called when the HUD appeared.
+	@param _hud The hud which did appear.
+ */
+- (void)hudDidAppear:(ATMHud *)_hud;
+
+/**
+	Called before the HUD updates.
+	@param _hud The hud which will update.
+ */
+- (void)hudWillUpdate:(ATMHud *)_hud;
+
+/**
+	Called when the HUD updated.
+	@param _hud The hud which did update.
+ */
+- (void)hudDidUpdate:(ATMHud *)_hud;
+
+/**
+	Called before the HUD disappears.
+	@param _hud The hud which will disappear.
+ */
+- (void)hudWillDisappear:(ATMHud *)_hud;
+
+/**
+	Called when the HUD disappeared.
+	@param _hud The hud which did disappear.
+ */
+- (void)hudDidDisappear:(ATMHud *)_hud;
+
+
+@end
