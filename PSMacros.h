2d4613427729a46c5fbe9742ced95a74f7de06db
Matthias Tretter <matthias.tretter@gmail.com>
2010-12-23 17:11:05 +0100

    fixed error in C-Macro SETTINGS_TEXT_COLOR

--- PSMacros.h
+++ PSMacros.h
@@ -22,7 +22,7 @@ static inline BOOL IsEmpty(id thing) {
 #define CGRectClearCoords(_CGRECT) PSRectClearCoords(_CGRECT) // legacy
 
 // color
-#define SETTINGS_TEXT_COLOR	RGB(57, 85, 135
+#define SETTINGS_TEXT_COLOR	RGBCOLOR(57, 85, 135)
 #define RGBCOLOR(r,g,b) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:1]
 #define XHEXCOLOR(c) [UIColor colorWithRed:((c>>16)&0xFF)/255.0 green:((c>>8)&0xFF)/255.0 blue:(c&0xFF)/255.0 alpha:1.0]
 #define XHEXCOLOR_ALPHA(c, a) [UIColor colorWithRed:((c>>16)&0xFF)/255.0 green:((c>>8)&0xFF)/255.0 blue:(c&0xFF)/255.0 alpha:a]
