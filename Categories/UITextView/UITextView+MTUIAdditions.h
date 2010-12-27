ce7c799f565ed77b2c1dab183b98011e5952083b
Matthias Tretter <matthias.tretter@gmail.com>
2010-12-27 21:05:02 +0100

    Added category on UITextView: size to fit needed height

---
+++ Categories/UITextView/UITextView+MTUIAdditions.h
@@ -0,0 +1,17 @@
+//
+//  UITextView+MTUIAdditions.h
+//  PSFoundation
+//
+//  Created by Matthias Tretter on 27.12.10.
+//  Copyright 2010 Peter Steinberger. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UITextView (MTUIAdditions)
+
+- (void) sizeToFitNeededHeight;
+
+@end
+
+
