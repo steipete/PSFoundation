099ce47b584fb9f97905b4a079bd1ef16a138310
Peter Steinberger <me@petersteinberger.com>
2011-03-08 15:23:53 +0100

    improve IsEqual macro for stuff like CoreData

--- PSMacros.h
+++ PSMacros.h
@@ -13,6 +13,7 @@
 // http://www.wilshipley.com/blog/2005/10/pimp-my-code-interlude-free-code.html
 static inline BOOL IsEmpty(id thing) {
 	return thing == nil ||
+  ([thing isEqual:[NSNull null]]) ||
   ([thing respondsToSelector:@selector(length)] && [(NSData *)thing length] == 0) ||
   ([thing respondsToSelector:@selector(count)]  && [(NSArray *)thing count] == 0);
 }
