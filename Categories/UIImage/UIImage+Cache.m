9f4cccba7d93a18d87cfd6dbef207d844c042c7d
Peter Steinberger <me@petersteinberger.com>
2011-04-12 02:46:37 +0200

    reformat cache, add logs

--- Categories/UIImage/UIImage+Cache.m
+++ Categories/UIImage/UIImage+Cache.m
@@ -13,46 +13,47 @@ static NSMutableDictionary *UIImageCache;
 @implementation UIImage(Cache)
 
 - (id)initWithContentsOfResolutionIndependentFile:(NSString *)path {
-  if ( [UIScreen instancesRespondToSelector:@selector(scale)] && (int)[[UIScreen mainScreen] scale] == 2.0 ) {
-    NSString *path2x = [[path stringByDeletingLastPathComponent]
-                        stringByAppendingPathComponent:[NSString stringWithFormat:@"%@@2x.%@",
-                                                        [[path lastPathComponent] stringByDeletingPathExtension],
-                                                        [path pathExtension]]];
-    
-    if ( [[NSFileManager defaultManager] fileExistsAtPath:path2x] ) {
-      return [self initWithContentsOfFile:path2x];
+    if ( [UIScreen instancesRespondToSelector:@selector(scale)] && (int)[[UIScreen mainScreen] scale] == 2.0 ) {
+        NSString *path2x = [[path stringByDeletingLastPathComponent]
+                            stringByAppendingPathComponent:[NSString stringWithFormat:@"%@@2x.%@",
+                                                            [[path lastPathComponent] stringByDeletingPathExtension],
+                                                            [path pathExtension]]];
+
+        if ( [[NSFileManager defaultManager] fileExistsAtPath:path2x] ) {
+            return [self initWithContentsOfFile:path2x];
+        }
     }
-  }
-  
-  return [self initWithContentsOfFile:path];
+
+    return [self initWithContentsOfFile:path];
 }
 
 + (UIImage*)imageWithContentsOfResolutionIndependentFile:(NSString *)path {
-  return [[[UIImage alloc] initWithContentsOfResolutionIndependentFile:path] autorelease];
+    return [[[UIImage alloc] initWithContentsOfResolutionIndependentFile:path] autorelease];
 }
 
 
 + (id)cachedImageWithContentsOfFile:(NSString *)path {
-  id result = nil;
-  @synchronized(UIImageCache) {
-    if (!UIImageCache)
-      UIImageCache = [[NSMutableDictionary alloc] init];
-    else {
-      result = [UIImageCache objectForKey:path];
-      if (result)
-        return result;
+    id result = nil;
+    @synchronized(UIImageCache) {
+        if (!UIImageCache)
+            UIImageCache = [[NSMutableDictionary alloc] init];
+        else {
+            result = [UIImageCache objectForKey:path];
+            if (result)
+                return result;
+        }
+        result = [[[UIImage alloc] initWithContentsOfResolutionIndependentFile:path] autorelease];
+        if(result) [UIImageCache setObject:result forKey:path];
     }
-    result = [[[UIImage alloc] initWithContentsOfResolutionIndependentFile:path] autorelease];
-    if(result) [UIImageCache setObject:result forKey:path];
-  }
-  return result;
+    return result;
 }
 
 
 + (void)clearCache {
-  @synchronized(UIImageCache) {
-    [UIImageCache removeAllObjects];
-  }
+    @synchronized(UIImageCache) {
+        DDLogInfo(@"Clearing image cache: %d objects", [UIImageCache count]);
+        [UIImageCache removeAllObjects];
+    }
 }
 
 @end
