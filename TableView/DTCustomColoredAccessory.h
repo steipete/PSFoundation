3aff8db852023b5359f321d78be3033cb93f783c
Matthias Tretter <matthias.tretter@gmail.com>
2011-04-12 17:36:16 +0200

    Updated custom colored accessory

--- TableView/DTCustomColoredAccessory.h
+++ TableView/DTCustomColoredAccessory.h
@@ -6,18 +6,32 @@
 //  Copyright 2011 @myell0w. All rights reserved.
 //
 //  Found on cocoanetics.com
+//  http://www.cocoanetics.com/2011/03/expandingcollapsing-tableview-sections/
 
 #import <Foundation/Foundation.h>
 
 
-@interface DTCustomColoredAccessory : UIControl {
+typedef enum
+{
+    DTCustomColoredAccessoryTypeRight = 0,
+    DTCustomColoredAccessoryTypeUp,
+    DTCustomColoredAccessoryTypeDown
+} DTCustomColoredAccessoryType;
+
+@interface DTCustomColoredAccessory : UIControl
+{
 	UIColor *_accessoryColor;
 	UIColor *_highlightedColor;
+
+    DTCustomColoredAccessoryType _type;
 }
 
 @property (nonatomic, retain) UIColor *accessoryColor;
 @property (nonatomic, retain) UIColor *highlightedColor;
 
+@property (nonatomic, assign)  DTCustomColoredAccessoryType type;
+
 + (DTCustomColoredAccessory *)accessoryWithColor:(UIColor *)color;
++ (DTCustomColoredAccessory *)accessoryWithColor:(UIColor *)color type:(DTCustomColoredAccessoryType)type;
 
 @end
--- TableView/DTCustomColoredAccessory.m
+++ TableView/DTCustomColoredAccessory.m
@@ -26,27 +26,74 @@
     [super dealloc];
 }
 
-+ (DTCustomColoredAccessory *)accessoryWithColor:(UIColor *)color
++ (DTCustomColoredAccessory *)accessoryWithColor:(UIColor *)color {
+	return [self accessoryWithColor:color type:DTCustomColoredAccessoryTypeRight];
+}
+
++ (DTCustomColoredAccessory *)accessoryWithColor:(UIColor *)color type:(DTCustomColoredAccessoryType)type
 {
-	DTCustomColoredAccessory *ret = [[[DTCustomColoredAccessory alloc] initWithFrame:CGRectMake(0, 0, 11.0, 15.0)] autorelease];
+	DTCustomColoredAccessory *ret = [[[DTCustomColoredAccessory alloc] initWithFrame:CGRectMake(0, 0, 15.0, 15.0)] autorelease];
 	ret.accessoryColor = color;
+    ret.type = type;
 
 	return ret;
 }
 
 - (void)drawRect:(CGRect)rect
 {
-	// (x,y) is the tip of the arrow
-	CGFloat x = CGRectGetMaxX(self.bounds)-3.0;;
-	CGFloat y = CGRectGetMidY(self.bounds);
-	const CGFloat R = 4.5;
-	CGContextRef ctxt = UIGraphicsGetCurrentContext();
-	CGContextMoveToPoint(ctxt, x-R, y-R);
-	CGContextAddLineToPoint(ctxt, x, y);
-	CGContextAddLineToPoint(ctxt, x-R, y+R);
-	CGContextSetLineCap(ctxt, kCGLineCapSquare);
-	CGContextSetLineJoin(ctxt, kCGLineJoinMiter);
-	CGContextSetLineWidth(ctxt, 3);
+    CGContextRef ctxt = UIGraphicsGetCurrentContext();
+
+    const CGFloat R = 4.5;
+
+    switch (_type)
+    {
+        case DTCustomColoredAccessoryTypeRight:
+        {
+            // (x,y) is the tip of the arrow
+            CGFloat x = CGRectGetMaxX(self.bounds)-3.0;;
+            CGFloat y = CGRectGetMidY(self.bounds);
+
+            CGContextMoveToPoint(ctxt, x-R, y-R);
+            CGContextAddLineToPoint(ctxt, x, y);
+            CGContextAddLineToPoint(ctxt, x-R, y+R);
+
+            break;
+        }
+
+        case DTCustomColoredAccessoryTypeUp:
+        {
+            // (x,y) is the tip of the arrow
+            CGFloat x = CGRectGetMaxX(self.bounds)-7.0;;
+            CGFloat y = CGRectGetMinY(self.bounds)+5.0;
+
+            CGContextMoveToPoint(ctxt, x-R, y+R);
+            CGContextAddLineToPoint(ctxt, x, y);
+            CGContextAddLineToPoint(ctxt, x+R, y+R);
+
+            break;
+        }
+
+        case DTCustomColoredAccessoryTypeDown:
+        {
+            // (x,y) is the tip of the arrow
+            CGFloat x = CGRectGetMaxX(self.bounds)-7.0;;
+            CGFloat y = CGRectGetMaxY(self.bounds)-5.0;
+
+            CGContextMoveToPoint(ctxt, x-R, y-R);
+            CGContextAddLineToPoint(ctxt, x, y);
+            CGContextAddLineToPoint(ctxt, x+R, y-R);
+
+            break;
+        }
+
+
+        default:
+            break;
+    }
+
+    CGContextSetLineCap(ctxt, kCGLineCapSquare);
+    CGContextSetLineJoin(ctxt, kCGLineJoinMiter);
+    CGContextSetLineWidth(ctxt, 3);
 
 	if (self.highlighted)
 	{
@@ -89,5 +136,6 @@
 
 @synthesize accessoryColor = _accessoryColor;
 @synthesize highlightedColor = _highlightedColor;
+@synthesize type = _type;
 
 @end
