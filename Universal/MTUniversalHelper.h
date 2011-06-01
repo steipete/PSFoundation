61d1509e7e38f5213028a296d058af1e47b2c948
Matthias Tretter <matthias.tretter@gmail.com>
2011-06-01 11:28:54 +0200

    Fixed typo in MTPointMake (silly me)

--- Universal/MTUniversalHelper.h
+++ Universal/MTUniversalHelper.h
@@ -19,7 +19,7 @@
 #define MTRectMake(x1,y1,w1,h1,x2,y2,w2,h2) (isIPad() ? CGRectMake(x2,y2,w2,h2) : CGRectMake(x1,y1,w1,h1))
 
 // creates a point with the first 2 parameters on iPhone and the second 2 parameters on iPad
-#define MTPointMake(x1,y1,x2,y2)    (isIPad() ? CGPointMake(x2,y2) : CGPointMake(x1,x2))
+#define MTPointMake(x1,y1,x2,y2)    (isIPad() ? CGPointMake(x2,y2) : CGPointMake(x1,y1))
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 #pragma mark -
