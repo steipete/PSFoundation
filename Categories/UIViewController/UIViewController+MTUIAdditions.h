2107ed9bff1b77b02ae9d786df5efe4c328e90e0
Matthias Tretter <matthias.tretter@gmail.com>
2011-06-01 12:19:51 +0200

    Added Category on UIViewController to show/hide a big ActivityIndicator centered in the view

---
+++ Categories/UIViewController/UIViewController+MTUIAdditions.h
@@ -0,0 +1,19 @@
+//
+//  UIViewController+MTUIAdditions.h
+//  PSFoundation
+//
+//  Created by Matthias Tretter on 01.06.11.
+//  Copyright 2011 @myell0w. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface UIViewController (MTUIAdditions)
+
+// shows a big loading indicator centered in the view
+- (void)showLoadingIndicator;
+// hides the loading indicator and removes it from the view-hierarchy
+- (void)hideLoadingIndicator;
+
+@end
---
+++ Categories/UIViewController/UIViewController+MTUIAdditions.m
@@ -0,0 +1,36 @@
+//
+//  UIViewController+MTUIAdditions.m
+//  PSFoundation
+//
+//  Created by Matthias Tretter on 01.06.11.
+//  Copyright 2011 @myell0w. All rights reserved.
+//
+
+#import "UIViewController+MTUIAdditions.h"
+
+#define kMTActivityViewTag      34032
+
+
+@implementation UIViewController (MTUIAdditions)
+
+- (void)showLoadingIndicator {
+    UIActivityIndicatorView *activityView = [[[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge] autorelease];
+    
+    activityView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleLeftMargin;
+    activityView.center = self.view.center;
+    activityView.tag = kMTActivityViewTag;
+    
+    [self.view addSubview:activityView];
+}
+
+- (void)hideLoadingIndicator {
+    id activityView = [self.view viewWithTag:kMTActivityViewTag];
+    
+    if ([activityView isKindOfClass:[UIActivityIndicatorView class]]) {
+        [activityView stopAnimating];
+    }
+    
+    [activityView removeFromSuperview];
+}
+
+@end
