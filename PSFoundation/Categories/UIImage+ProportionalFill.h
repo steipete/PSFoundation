e9f05e9ceec6efcb6a870584dd9672152d073ffd
Zachary Waldowski <zwaldowski@gmail.com>
2011-06-03 00:38:48 -0400

    Deprecate UIImage+ProportionalFill in favor of NYXImageUtilities.

--- PSFoundation/Categories/UIImage+ProportionalFill.h
+++ PSFoundation/Categories/UIImage+ProportionalFill.h
@@ -1,14 +1,14 @@
 //
 //  UIImage+ProportionalFill.h
+//  PSFoundation
 //
-//  Created by Matt Gemmell on 20/08/2008.
-//  Copyright 2008 Instinctive Code.
+//  Uses method names inspired by Matt Gemmell.
 //
-// http://mattgemmell.com/2010/07/05/mgimageutilities
+//  References:
+//   - http://mattgemmell.com/2010/07/05/mgimageutilities
+//
 
-#import <UIKit/UIKit.h>
-
-@interface UIImage (MGProportionalFill)
+@interface UIImage (ProportionalFill)
 
 typedef enum {
     MGImageResizeCrop,	// analogous to UIViewContentModeScaleAspectFill, i.e. "best fit" with no space around.
@@ -18,9 +18,9 @@ typedef enum {
 } MGImageResizingMethod;
 
 // modified by @steipete
-- (UIImage *)imageToFitSize:(CGSize)fitSize method:(MGImageResizingMethod)resizeMethod honorScaleFactor:(BOOL)honorScaleFactor;
-- (UIImage *)imageToFitSize:(CGSize)fitSize method:(MGImageResizingMethod)resizeMethod;
-- (UIImage *)imageCroppedToFitSize:(CGSize)size; // uses MGImageResizeCrop
-- (UIImage *)imageScaledToFitSize:(CGSize)size; // uses MGImageResizeScale
+- (UIImage *)imageToFitSize:(CGSize)fitSize method:(MGImageResizingMethod)resizeMethod honorScaleFactor:(BOOL)honorScaleFactor DEPRECATED_ATTRIBUTE;
+- (UIImage *)imageToFitSize:(CGSize)fitSize method:(MGImageResizingMethod)resizeMethod DEPRECATED_ATTRIBUTE;
+- (UIImage *)imageCroppedToFitSize:(CGSize)size DEPRECATED_ATTRIBUTE; // uses MGImageResizeCrop
+- (UIImage *)imageScaledToFitSize:(CGSize)size DEPRECATED_ATTRIBUTE; // uses MGImageResizeScale
 
 @end
--- PSFoundation/Categories/UIImage+ProportionalFill.m
+++ PSFoundation/Categories/UIImage+ProportionalFill.m
@@ -1,133 +1,47 @@
 //
 //  UIImage+ProportionalFill.m
+//  PSFoundation
 //
-//  Created by Matt Gemmell on 20/08/2008.
-//  Copyright 2008 Instinctive Code.
-//  Heavily fixed and modified by Peter Steinberger 24/04/2010
+//  Uses method names inspired by Matt Gemmell.
 //
 
 #import "UIImage+ProportionalFill.h"
-#import "PSMacros.h"
+#import "NYXImagesUtilities.h"
 
-@implementation UIImage (MGProportionalFill)
+@implementation UIImage (ProportionalFill)
 
 - (UIImage *)imageToFitSize:(CGSize)fitSize method:(MGImageResizingMethod)resizeMethod honorScaleFactor:(BOOL)honorScaleFactor {
-    
-	float imageScaleFactor = 1.0;
-    if (honorScaleFactor) {
-        imageScaleFactor = [self scale];
-    }
-    
-    float sourceWidth = [self size].width * imageScaleFactor;
-    float sourceHeight = [self size].height * imageScaleFactor;
-    float targetWidth = fitSize.width;
-    float targetHeight = fitSize.height;
-    BOOL cropping = !(resizeMethod == MGImageResizeScale);
-    
-    // adapt rect based on source image size
-    //DDLogInfo(@"imageOrientation: %d", self.imageOrientation);
-    switch (self.imageOrientation) {
-        case UIImageOrientationLeft:    // button top
-        case UIImageOrientationRight:   // button bottom
-        case UIImageOrientationLeftMirrored:
-        case UIImageOrientationRightMirrored: { 
-            ps_swapf(sourceWidth, sourceHeight);
-            ps_swapf(targetWidth, targetHeight);
-        }break;
-            
-        case UIImageOrientationUp:     // button left
-        case UIImageOrientationDown:   // button right
-        default: {             // works in default
-        }break;
-    }
-    
-    // Calculate aspect ratios
-    float sourceRatio = sourceWidth / sourceHeight;
-    float targetRatio = targetWidth / targetHeight;
-    
-    // Determine what side of the source image to use for proportional scaling
-    BOOL scaleWidth = (sourceRatio <= targetRatio);
-    // Deal with the case of just scaling proportionally to fit, without cropping
-    scaleWidth = (cropping) ? scaleWidth : !scaleWidth;
-    
-    // Proportionally scale source image
-    float scalingFactor, scaledWidth, scaledHeight;
-    if (scaleWidth) {
-        scalingFactor = 1.0 / sourceRatio;
-        scaledWidth = targetWidth;
-        scaledHeight = round(targetWidth * scalingFactor);
-    } else {
-        scalingFactor = sourceRatio;
-        scaledWidth = round(targetHeight * scalingFactor);
-        scaledHeight = targetHeight;
-    }
-    float scaleFactor = scaledHeight / sourceHeight;
-    
-    // Calculate compositing rectangles
-    CGRect sourceRect, destRect;
-    if (cropping) {
-        destRect = CGRectMake(0, 0, targetWidth, targetHeight);
-        float destX, destY;
-        if (resizeMethod == MGImageResizeCrop) {
-            // Crop center
-            destX = round((scaledWidth - targetWidth) / 2.0);
-            destY = round((scaledHeight - targetHeight) / 2.0);
-        } else if (resizeMethod == MGImageResizeCropStart) {
-            // Crop top or left (prefer top)
-            if (scaleWidth) {
-                // Crop top
-                destX = 0.0;
-                destY = 0.0;
-            } else {
-                // Crop left
-                destX = 0.0;
-                destY = round((scaledHeight - targetHeight) / 2.0);
-            }
-        } else if (resizeMethod == MGImageResizeCropEnd) {
-            // Crop bottom or right
-            if (scaleWidth) {
-                // Crop bottom
-                destX = round((scaledWidth - targetWidth) / 2.0);
-                destY = round(scaledHeight - targetHeight);
-            } else {
-                // Crop right
-                destX = round(scaledWidth - targetWidth);
-                destY = round((scaledHeight - targetHeight) / 2.0);
-            }
-        }
-        sourceRect = CGRectMake(destX / scaleFactor, destY / scaleFactor,
-                                targetWidth / scaleFactor, targetHeight / scaleFactor);
-    } else {
-        sourceRect = CGRectMake(0, 0, sourceWidth, sourceHeight);
-        destRect = CGRectMake(0, 0, scaledWidth, scaledHeight);
-    }
-    
-    // Create appropriately modified image.
-    UIImage *image = nil;
-    UIGraphicsBeginImageContextWithOptions(destRect.size, NO, honorScaleFactor ? 0.0 : 1.0); // 0.0 for scale means "correct scale for device's main screen".
-    CGImageRef sourceImg = CGImageCreateWithImageInRect([self CGImage], sourceRect); // cropping happens here.
-    image = [UIImage imageWithCGImage:sourceImg scale:0.0 orientation:self.imageOrientation]; //  create cropped UIImage.
-    //DDLogInfo(@"image size: %@", NSStringFromCGSize(image.size));
-    [image drawInRect:destRect]; // the actual scaling happens here, and orientation is taken care of automatically.
-    CGImageRelease(sourceImg);
-    image = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    
-    return image;
+    if (resizeMethod == MGImageResizeScale)
+        return [self scaleToFitSize:fitSize];
+    else if (resizeMethod == MGImageResizeCrop)
+        return [self cropToSize:fitSize];
+    else if (resizeMethod == MGImageResizeCropStart)
+        return [self cropToSize:fitSize usingMode:NYXCropModeTopLeft];
+    else if (resizeMethod == MGImageResizeCropEnd)
+        return [self cropToSize:fitSize usingMode:NYXCropModeBottomRight];
+    return self;
 }
 
 - (UIImage *)imageToFitSize:(CGSize)fitSize method:(MGImageResizingMethod)resizeMethod {
-    return [self imageToFitSize:fitSize method:resizeMethod honorScaleFactor:YES];
+    if (resizeMethod == MGImageResizeScale)
+        return [self scaleToFitSize:fitSize];
+    else if (resizeMethod == MGImageResizeCrop)
+        return [self cropToSize:fitSize];
+    else if (resizeMethod == MGImageResizeCropStart)
+        return [self cropToSize:fitSize usingMode:NYXCropModeTopLeft];
+    else if (resizeMethod == MGImageResizeCropEnd)
+        return [self cropToSize:fitSize usingMode:NYXCropModeBottomRight];
+    return self;
 }
 
 
 - (UIImage *)imageCroppedToFitSize:(CGSize)fitSize {
-    return [self imageToFitSize:fitSize method:MGImageResizeCrop honorScaleFactor:YES];
+    return [self cropToSize:fitSize];
 }
 
 
 - (UIImage *)imageScaledToFitSize:(CGSize)fitSize {
-    return [self imageToFitSize:fitSize method:MGImageResizeScale honorScaleFactor:YES];
+    return [self scaleToFitSize:fitSize];
 }
 
 
