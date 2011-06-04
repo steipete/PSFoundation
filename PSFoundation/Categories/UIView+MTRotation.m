e1d4b54920bdf41f8aefc35dc10fea99d0b6e7e9
Zachary Waldowski <zwaldowski@gmail.com>
2011-06-04 14:28:06 -0400

    Move MTRotation and add *.orig to gitignore.

---
+++ PSFoundation/Categories/UIView+MTRotation.m
@@ -0,0 +1,92 @@
+//
+//  UIView+MTRotation.m
+//  TVThek
+//
+//  Created by Matthias Tretter on 27.05.11.
+//  Copyright 2011 NOUS Wissensmanagement GmbH. All rights reserved.
+//
+
+#import "UIView+MTRotation.h"
+#import <objc/runtime.h>
+
+////////////////////////////////////////////////////////////////////////
+#pragma mark -
+#pragma mark Keys for associated objects
+////////////////////////////////////////////////////////////////////////
+
+static char portraitFrameKey;
+static char landscapeFrameKey;
+
+
+@implementation UIView (MTRotation)
+
+////////////////////////////////////////////////////////////////////////
+#pragma mark -
+#pragma mark UIView+MTRotation
+////////////////////////////////////////////////////////////////////////
+
+- (BOOL)hasPortraitAndLandscapeFrames {
+    return !CGRectIsEmpty(self.portraitFrame) && !CGRectIsEmpty(self.landscapeFrame);
+}
+
+- (CGRect)portraitFrame {
+    // read associated object for portrait frame
+    NSValue *portraitFrameValue = (NSValue *)objc_getAssociatedObject(self, &portraitFrameKey);
+    
+    if (portraitFrameValue != nil) {
+        return [portraitFrameValue CGRectValue];
+    } else {
+        return CGRectZero;
+    }
+}
+
+- (CGRect)landscapeFrame {
+    // read associated object for landscape frame
+    NSValue *landscapeFrameValue = (NSValue *)objc_getAssociatedObject(self, &landscapeFrameKey);
+    
+    if (landscapeFrameValue != nil) {
+        return [landscapeFrameValue CGRectValue];
+    } else {
+        return CGRectZero;
+    }
+}
+
+- (void)setPortraitFrame:(CGRect)portraitFrame {
+    NSValue *portraitFrameValue = [NSValue valueWithCGRect:portraitFrame];
+    objc_setAssociatedObject(self, &portraitFrameKey, portraitFrameValue, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (void)setLandscapeFrame:(CGRect)landscapeFrame {
+    NSValue *landscapeFrameValue = [NSValue valueWithCGRect:landscapeFrame];
+    objc_setAssociatedObject(self, &landscapeFrameKey, landscapeFrameValue, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (void)animateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration {
+    [UIView animateWithDuration:duration animations:^{
+        [self setFrameForInterfaceOrientation:toInterfaceOrientation];
+    }];
+}
+
+- (void)setFrameForInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    if (UIInterfaceOrientationIsPortrait(interfaceOrientation)) {
+        if (!CGRectIsEmpty(self.portraitFrame)) {
+            self.frame = self.portraitFrame;
+        }
+    } else {
+        if (!CGRectIsEmpty(self.landscapeFrame)) {
+            self.frame = self.landscapeFrame;
+        }
+    }
+}
+
+- (void)animateSubviewsToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration {
+    [UIView animateWithDuration:duration animations:^{
+        for (UIView *view in self.subviews) {
+            if (view.hasPortraitAndLandscapeFrames) {
+                [view setFrameForInterfaceOrientation:toInterfaceOrientation];
+            }
+        }
+    }];
+}
+
+@end
